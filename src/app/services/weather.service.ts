20404659250a9318288aa2c1e42c596f2c9950f8
Palle Madhu <madhupalle2311@gmail.com>
2023-09-30 13:12:09 +0530

    commit files after conflicts

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -11,7 +11,8 @@ export class WeatherService {
 
   constructor(private http: HttpClient) {}
 
-  getWeather(city:string): Observable<weatherData>{
-    return this.http.get<weatherData>(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}`);
-  }
+  getWeather(city: string): Observable<weatherData> {
+    return this.http.get<weatherData>(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`
+  )}
 }
